4076ec5a5f8f1fbb876c2af72a8a5e7ea840aa45
phebemar <166551031+phebemar@users.noreply.github.com>
2024-05-21 13:07:40 -0700

    Update my_library.py

--- my_library.py
+++ my_library.py
@@ -10,7 +10,7 @@ def cond_prob(table,evidence,evidence_value,target,target_value):
   t_subset = up_table_subset(table, target, 'equals', target_value)
   e_list = up_get_column(t_subset, evidence)
   p_b_a = sum([1 if v==evidence_value else 0 for v in e_list])/len(e_list)
-  return p_b_a
+  return p_b_a + .01
 
 def cond_probs_product(table, evidence_row, target, target_value):
   table_columns=up_list_column_names(table)

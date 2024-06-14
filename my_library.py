20cdd72c2ad648aa0ffafb66911d1feb6e4d9f99
phebemar <166551031+phebemar@users.noreply.github.com>
2024-06-13 23:03:12 -0700

    Update my_library.py

--- my_library.py
+++ my_library.py
@@ -98,3 +98,29 @@ def run_random_forest(train, test, target, n):
 
   metrics_table = up_metrics_table(all_mets)
   return metrics_table
+
+
+
+def try_archs(full_table, target, architectures, thresholds):
+  #target is target column name
+
+  #split full_table
+  train_table, test_table = up_train_test_split(full_table, target,.4 )
+
+  #now loop through architectures
+  for architecture in architectures:
+    all_results = up_neural_net(train_table, test_table, architecture, target)
+
+    #loop through thresholds
+    all_mets = []
+    for t in thresholds:
+        all_predictions = [1 if pos>=t else 0 for neg,pos in all_results]
+        pred_act_list = up_zip_lists(all_predictions, up_get_column(test_table, target))
+        mets = metrics(pred_act_list)
+        mets['Threshold'] = t
+        all_mets = all_mets + [mets]
+
+    print(f'Architecture: {architecture}')
+    print(up_metrics_table(all_mets))
+
+  return None
